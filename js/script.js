c8e82d3f8a51a27f43a3afa168231254155f1f59
João Vitor Alves <joaovtoralves@gmail.com>
2023-05-26 15:53:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@ function renda(){
     let sm = 1980
     let nascimento = new Date( document.getElementById('nasc').value);
     let hoje = new Date(); 
-    let anoscompletos = hoje.getUTCFullYear()- nascimento.getUTCFullYear(nascimento);
+    let anoscompletos = hoje.getUTCFullYear()- nascimento.getUTCFullYear();
     let mes = new Date().getMonth() - nascimento.getMonth()
     if (mes < 0 || ( mes === 0 && hoje.getDate() < nasc.getDate() ) ) {
         anoscompletos--
     }
     let quantidadepessoas = document.getElementById('qtdp').value;
     let somasalarios = document.getElementById('qtds').value;
-    if(anoscompletos>=16 && somasalarios <= parseFloat((1.5*sm)/quantidadepessoas)){
+    let rendapercapita = sm / quantidadepessoas
+    if(anoscompletos>=16 && rendapercapita <= sm){
         document.getElementById('decisao').innerHTML = alertap
     }
     else{
