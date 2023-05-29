84717a761affac91496220e2a0f8b379adbd4e14
João Vitor Alves <joaovtoralves@gmail.com>
2023-05-29 14:38:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function renda(){
-    
+
     let sm = 1980
     let nascimento = new Date( document.getElementById('nasc').value);
     let hoje = new Date(); 
@@ -12,7 +12,7 @@ function renda(){
     let somasalarios = document.getElementById('qtds').value;
     let rendapercapita = somasalarios / quantidadepessoas
     if(anoscompletos>=16 && rendapercapita <= sm){
-        document.getElementById('decisao').innerHTML = "aprovado, você atende aos critérios para se matricular no programa,entre em contato com o número (xx) xxxx-xxxx para fianlizar sua matricula "
+        document.getElementById('decisao').innerHTML = "aprovado, você atende aos critérios para se matricular no programa,entre em contato com o número (xx) xxxx-xxxx para finalizar sua matricula "
     }
     else{
         document.getElementById('decisao').innerHTML = "reprovado, você não atende aos critérios para participar do programa "
